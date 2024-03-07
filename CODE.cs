10b0e2d5c1789f4bd7e676cbf3a1ab070bb7616f
OldM1lk <144899273+OldM1lk@users.noreply.github.com>
2024-03-07 10:01:38 +0700

    Update CODE.cs

--- CODE.cs
+++ CODE.cs
@@ -5,17 +5,17 @@ using System.Text.RegularExpressions;
 
 namespace StringsAndCollections {
   class Program {
-    static string ReplacePhoneNumber(string input, Match match) {
+    static string replacePhoneNumber(string input, Match match) {
       string phoneNumber = match.Value;
       string replacedPhoneNumber = "+380 " + phoneNumber.Substring(2, 2) + " " + phoneNumber.Substring(6, 3)
                                    + " " + phoneNumber.Substring(10, 2) + " " + phoneNumber.Substring(13, 2);
-      
+
       return input.Replace(match.Value, replacedPhoneNumber);
     }
 
     static void Main(string[] args) {
       Console.Write("Введите директорию: ");
-      string directoryPath = Console.ReadLine();
+      string directory = Console.ReadLine();
       Dictionary<string, string> wrongWords = new Dictionary<string, string> {
         {"привте", "привет"},
         {"притев", "привет"},
@@ -39,19 +39,19 @@ namespace StringsAndCollections {
         {"прявет", "привет"}
       };
       string numberToFind = @"\((\d{3})\) (\d{3})-(\d{2})-(\d{2})";
-      string[] filesInPath = Directory.GetFiles(directoryPath, "*.txt");
+      string[] filesInDirectory = Directory.GetFiles(directory, "*.txt");
 
-      foreach (string file in filesInPath) {
+      foreach (string file in filesInDirectory) {
         string fileContent = File.ReadAllText(file);
-        
+
         foreach (KeyValuePair<string, string> wrongWord in wrongWords) {
           fileContent = fileContent.Replace(wrongWord.Key, wrongWord.Value);
         }
 
-        MatchCollection matches = Regex.Matches(fileContent, numberToFind);
+        MatchCollection numberMatches = Regex.Matches(fileContent, numberToFind);
 
-        foreach (Match match in matches) {
-          fileContent = ReplacePhoneNumber(fileContent, match);
+        foreach (Match numberMatch in numberMatches) {
+          fileContent = replacePhoneNumber(fileContent, numberMatch);
         }
 
         File.WriteAllText(file, fileContent);
